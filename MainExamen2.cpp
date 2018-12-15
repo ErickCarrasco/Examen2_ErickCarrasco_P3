3880d0318e7ffeab18907027b81ade49d2dc10c3
Erick Carrasco <erick.e.carrasco88@gmail.com>
2018-12-14 18:48:55 -0600

    Se esta mejorando la clase de log in

--- MainExamen2.cpp
+++ MainExamen2.cpp
@@ -11,6 +11,9 @@ using std::endl;
 #include <string>
 using std::string;
 
+#include <sstream>
+using std::stringstream;
+
 
 
 int main(){
@@ -112,7 +115,7 @@ int main(){
 
 		}
 		if (seleccion==2){
-			//Leer Archivo Binario
+			/*
 			ofstream UsuariosW("Usuarios.dat", std::ios::binary);
 			for (int i = 0; i < numero; ++i){
 				Usuario* temp = stack->pop();
@@ -123,13 +126,113 @@ int main(){
 					temp=stack->pop();
 				}
 			}
-			UsuariosW.close();
-			Usuario* usuarioLectura= new Usuario();
-			ifstream usuarioR("Usuarios.dat", std::ios::binary);
-			usuarioLectura->read(usuarioR);
-			usuarioR.close();
+			*/
 
-			cout<<"Data leido: "<<endl<<usuarioLectura<<endl;
+			//Se ingresan los datos acumulados en el linked stack al archivo
+			//Luego se procedera a hacer log in
+			int sizeStack=stack->getSize();
+			ofstream outPutFile;
+			outPutFile.open("Test.txt", std::ios::app);
+			//NUMERO ES LA VARIABLE DEL SIZE DEL STACK
+			if(sizeStack!=0){
+				for (int i = 0; i < numero; ++i){
+					Usuario* temp = stack->pop();
+					while(temp!=NULL){
+						//cout<<temp->toString();
+						//temp->write(UsuariosW);
+						outPutFile<<temp;
+						delete temp;
+						temp=stack->pop();
+					}
+				}
+			}
+
+			//LECTURA PARA LOG IN
+			cout<<endl;
+			cout<<endl;
+			cout<<"|    ---- LOG IN -----    |"<<endl;
+			string user;
+			string contrauser;
+			cout<<endl;
+			cout<<"--->INGRESE UN NOMBRE DE USUARIO<---"<<endl;
+			getline(cin, user);
+			getline(cin, user);
+			cout<<endl;
+			cout<<"--->INGRESE LA CLAVE DE SU USER<---"<<endl;
+			getline(cin, contrauser);
+
+			//Lectura desde el archivo
+			bool validadorNombre=false;
+			bool validadorPass=false;
+			ifstream inputFile;
+			inputFile.open("Test.txt");
+			if(!inputFile.is_open()){
+				cout<<"Ocurrio un error con la base de datos"<<endl;
+			}else{
+				while(!inputFile.eof()){
+					string buffer;
+					getline(inputFile, buffer);
+					cout<<buffer<<endl;
+
+					int contador_comas=0;
+					int contador_comas2=0;
+					int numer_comas;
+					stringstream comparador_user;
+					stringstream comparador_clave;
+					bool validadorP1=true;
+					
+					for (int i = 0; i < buffer.size(); i++){
+
+						if (buffer[i]!=',' && validadorP1==true){
+							contador_comas++;
+
+						}
+						if (buffer[i]==','){
+							validadorP1=false;
+						}
+						
+					}
+					for (int j = 0; j < contador_comas; ++j){
+						comparador_user<<buffer[j];
+					}
+
+					/*
+					bool validadorP2=true;
+					for (int i = 0; i < buffer.size(); ++i){
+						if (numer_comas==3){
+							validadorP2=false;
+						}
+						if (validadorP2==true){
+							contador_comas2++;
+						}
+						if (buffer[i]==','){
+							numer_comas++;
+						}
+						
+					}
+
+					for (int j = contador_comas2; j < buffer.size(); ++j){
+						comparador_clave<<buffer[j];
+					}
+					*/
+
+					string comparador_user_s=comparador_user.str();
+					//string comparador_clave_s=comparador_clave.str();
+
+					//cout<<comparador_clave_s<<endl;
+					cout<<comparador_user_s<<endl;
+					if (comparador_user_s==user){
+						validadorNombre=true;
+					}
+				}
+				if (validadorNombre==true){
+					cout<<"Ha iniciado sesion"<<endl;
+				}
+			}
+
+
+
+
 		}
 
 		if (seleccion==3){
@@ -143,6 +246,6 @@ int main(){
 		}
 
 	}
-
+	delete stack;
 	return 0;
 }
